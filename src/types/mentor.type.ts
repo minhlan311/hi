53b5f0544c5abe311156f54ecf51af3cbaaaa2a0
Hai Tran <haitn2806@gmail.com>
2023-09-14 14:14:07 +0700

    fix-ts

---
+++ src/types/mentor.type.ts
@@ -0,0 +1,18 @@
+export type Register = {
+  fullName: string
+  email: string
+  password: string
+  confirmPassword: string
+  phoneNumber: string
+  birthDay: string
+}
+export type RegisterMentor = {
+  fullName: string
+  email: string
+  password: string
+  confirmPassword: string
+  phoneNumber: string
+  birthDay: string
+  certificates: string[]
+  userId: string
+}
