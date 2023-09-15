45815f193fbfe161624497c0df3ed4ecfde7d65b
Hai Tran <haitn2806@gmail.com>
2023-09-15 09:43:22 +0700

    response-footer

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,6 +1,5 @@
 import ucamFooter from '@/assets/icons/ucamFooter.png'
-import { Space, Typography } from 'antd'
-import '../../../assets/css/grid.css'
+import { Col, Row, Space, Typography } from 'antd'
 import './styles.scss'
 const { Text } = Typography
 
@@ -9,142 +8,142 @@ const Footer = () => {
     <>
       <div className='footer'>
         <div className='footer-col'>
-          <div className='grid '>
-            <div className='rowM gap-3'>
-              <div className='col-footer l-3 m-6 c-6' style={{ textAlign: 'start' }}>
-                <div className='col-footer-left'>
-                  <Space
-                    direction='vertical'
+          {/* <div className='grid '> */}
+          <Row justify={'center'} gutter={[20, 20]}>
+            <Col className='' style={{ textAlign: 'start' }}>
+              <div className='col-footer-left'>
+                <Space
+                  direction='vertical'
+                  style={{
+                    position: 'relative'
+                  }}
+                >
+                  <div className='mtz-footer-logo'>
+                    <img src={ucamFooter} />
+                  </div>
+
+                  <Space className='social-group'>
+                    <div style={{ textAlign: 'start' }}>{/* <DCMASVG /> */}</div>
+                  </Space>
+                </Space>
+              </div>
+            </Col>
+            <Col className=''>
+              <div className='col-footer-right max-w'>
+                <Space direction='vertical' style={{ textAlign: 'start' }}>
+                  <Text className='footer-heading text-yellow-heading'>VĂN PHÒNG TP HCM</Text>
+
+                  <div
                     style={{
-                      position: 'relative'
+                      display: 'flex',
+                      flexDirection: 'column',
+                      color: '#FFFFFF'
                     }}
                   >
-                    <div className='mtz-footer-logo'>
-                      <img src={ucamFooter} />
-                    </div>
-
-                    <Space className='social-group'>
-                      <div style={{ textAlign: 'start' }}>{/* <DCMASVG /> */}</div>
-                    </Space>
-                  </Space>
-                </div>
-              </div>
-              <div className='col-footer l-3 m-6 c-6'>
-                <div className='col-footer-right max-w'>
-                  <Space direction='vertical' style={{ textAlign: 'start' }}>
-                    <Text className='footer-heading text-yellow-heading'>VĂN PHÒNG TP HCM</Text>
-
-                    <div
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        color: '#FFFFFF'
-                      }}
-                    >
-                      <ul className='ul-fff'>
-                        <li>Địa chỉ: 286 Phạm Hùng, Hoà Phước, Hòa Vang, Đà Nẵng</li>
-                        <li>Hotline: 0359006655</li>
-                      </ul>
-                    </div>
-                  </Space>
-                </div>
-              </div>
-              <div className='col-footer l-3 m-6 c-6'>
-                <div className='col-footer-left max-w'>
-                  <Space direction='vertical' style={{ textAlign: 'start' }}>
-                    <Text className='footer-heading text-yellow-heading'>CHI NHÁNH ĐÀ NẴNG</Text>
                     <ul className='ul-fff'>
                       <li>Địa chỉ: 286 Phạm Hùng, Hoà Phước, Hòa Vang, Đà Nẵng</li>
                       <li>Hotline: 0359006655</li>
                     </ul>
-                  </Space>
-                </div>
+                  </div>
+                </Space>
               </div>
-              <div className='col-footer l-3 m-6 c-6'>
-                <div className='col-footer-right max-w'>
-                  <Space direction='vertical' style={{ textAlign: 'start' }}>
-                    <Text className='footer-heading text-yellow-heading'>CHI NHÁNH HÀ NỘI</Text>
-                    <ul className='ul-fff'>
-                      <li>Địa chỉ: 286 Phạm Hùng, Hoà Phước, Hòa Vang, Đà Nẵng</li>
-                      <li>Hotline: 035900665</li>
-                    </ul>
-                  </Space>
-                </div>
+            </Col>
+            <Col className=''>
+              <div className='col-footer-left max-w'>
+                <Space direction='vertical' style={{ textAlign: 'start' }}>
+                  <Text className='footer-heading text-yellow-heading'>CHI NHÁNH ĐÀ NẴNG</Text>
+                  <ul className='ul-fff'>
+                    <li>Địa chỉ: 286 Phạm Hùng, Hoà Phước, Hòa Vang, Đà Nẵng</li>
+                    <li>Hotline: 0359006655</li>
+                  </ul>
+                </Space>
               </div>
-            </div>
-          </div>
-          <div className='grid '>
-            <div className='rowM gap-3'>
-              <div className='col-footer l-3 m-6 c-6' style={{ textAlign: 'start' }}>
-                <div className='col-footer-left max-w'>
-                  <Space direction='vertical' style={{ textAlign: 'start' }}>
-                    <Text className='footer-heading text-white-heading'>Giới thiệu</Text>
+            </Col>
+            <Col className=''>
+              <div className='col-footer-right max-w'>
+                <Space direction='vertical' style={{ textAlign: 'start' }}>
+                  <Text className='footer-heading text-yellow-heading'>CHI NHÁNH HÀ NỘI</Text>
+                  <ul className='ul-fff'>
+                    <li>Địa chỉ: 286 Phạm Hùng, Hoà Phước, Hòa Vang, Đà Nẵng</li>
+                    <li>Hotline: 035900665</li>
+                  </ul>
+                </Space>
+              </div>
+            </Col>
+          </Row>
+          {/* </div> */}
 
-                    <div
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'column'
-                      }}
-                    >
-                      <ul className='ul-fff'>
-                        <li>Về chúng tôi</li>
-                        <li>Đội ngũ giáo viên</li>
-                        <li>Khách hàng và đối tác</li>
-                        <li>Tin tức</li>
-                      </ul>
-                    </div>
-                  </Space>
-                </div>
-              </div>
-              <div className='col-footer l-3 m-6 c-6'>
-                <div className='col-footer-right max-w' style={{ textAlign: 'start' }}>
-                  <Space direction='vertical' style={{ textAlign: 'start' }}>
-                    <Text className='footer-heading text-white-heading'>Tuyển dụng</Text>
+          <Row className='' justify={'center'} gutter={[10, 10]}>
+            <Col className='' style={{ textAlign: 'start' }}>
+              <div className='col-footer-left max-w'>
+                <Space direction='vertical' style={{ textAlign: 'start' }}>
+                  <Text className='footer-heading text-white-heading'>Giới thiệu</Text>
 
-                    <div
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'column'
-                      }}
-                    >
-                      <ul className='ul-fff'>
-                        <li>Become a Teacher</li>
-                        <li>Cơ hội việc làm</li>
-                        <li>Sổ tay nhân viên</li>
-                        <li>Góc học tập</li>
-                      </ul>
-                    </div>
-                  </Space>
-                </div>
-              </div>
-              <div className='col-footer l-3 m-6 c-6'>
-                <div className='col-footer-left max-w' style={{ textAlign: 'start' }}>
-                  <Space direction='vertical' style={{ textAlign: 'start' }}>
-                    <Text className='footer-heading text-white-heading'>Cung cấp giáo viên bản ngữ</Text>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column'
+                    }}
+                  >
                     <ul className='ul-fff'>
-                      <li>Mầm non & Tiểu học</li>
-                      <li>Trường THCS-THPT</li>
-                      <li>Đại học và sau đại học</li>
-                      <li>Trung tâm Anh Ngữ</li>
+                      <li>Về chúng tôi</li>
+                      <li>Đội ngũ giáo viên</li>
+                      <li>Khách hàng và đối tác</li>
+                      <li>Tin tức</li>
                     </ul>
-                  </Space>
-                </div>
+                  </div>
+                </Space>
               </div>
-              <div className='col-footer l-3 m-6 c-6'>
-                <div className='col-footer-right max-w' style={{ textAlign: 'start' }}>
-                  <Space direction='vertical' style={{ textAlign: 'start' }}>
-                    <Text className='footer-heading text-white-heading'>Khóa học tiếng Anh</Text>
+            </Col>
+            <Col className=''>
+              <div className='col-footer-right max-w' style={{ textAlign: 'start' }}>
+                <Space direction='vertical' style={{ textAlign: 'start' }}>
+                  <Text className='footer-heading text-white-heading'>Tuyển dụng</Text>
+
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column'
+                    }}
+                  >
                     <ul className='ul-fff'>
-                      <li>Tiếng anh 1 kèm 1</li>
-                      <li>Tiếng Anh cho trẻ em </li>
-                      <li>Tiếng Anh cho người đi làm</li>
-                      <li>Tiếng Anh Doanh nghiệp</li>
+                      <li>Become a Teacher</li>
+                      <li>Cơ hội việc làm</li>
+                      <li>Sổ tay nhân viên</li>
+                      <li>Góc học tập</li>
                     </ul>
-                  </Space>
-                </div>
+                  </div>
+                </Space>
               </div>
-            </div>
-          </div>
+            </Col>
+            <Col className=''>
+              <div className='col-footer-left max-w' style={{ textAlign: 'start' }}>
+                <Space direction='vertical' style={{ textAlign: 'start' }}>
+                  <Text className='footer-heading text-white-heading'>Cung cấp giáo viên bản ngữ</Text>
+                  <ul className='ul-fff'>
+                    <li>Mầm non & Tiểu học</li>
+                    <li>Trường THCS-THPT</li>
+                    <li>Đại học và sau đại học</li>
+                    <li>Trung tâm Anh Ngữ</li>
+                  </ul>
+                </Space>
+              </div>
+            </Col>
+            <Col className=''>
+              <div className='col-footer-right max-w' style={{ textAlign: 'start' }}>
+                <Space direction='vertical' style={{ textAlign: 'start' }}>
+                  <Text className='footer-heading text-white-heading'>Khóa học tiếng Anh</Text>
+                  <ul className='ul-fff'>
+                    <li>Tiếng anh 1 kèm 1</li>
+                    <li>Tiếng Anh cho trẻ em </li>
+                    <li>Tiếng Anh cho người đi làm</li>
+                    <li>Tiếng Anh Doanh nghiệp</li>
+                  </ul>
+                </Space>
+              </div>
+            </Col>
+          </Row>
+
           <hr className='hr-footer' />
           <div className='grid '>
             <div className='rowM flex-col-footer-bottom'>
