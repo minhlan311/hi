8dc0ae6aee115e7034a471e16c6f92774621b997
Hai Tran <haitn2806@gmail.com>
2023-10-02 13:55:10 +0700

    slug-call-api

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -67,9 +67,10 @@ export function formatHour(timeString: string) {
 }
 
 // convert để lấy id xuống từ url
-export function getIdFromUrl(url: string) {
-  const pathArr = url.split('/')
-  const id = pathArr[pathArr.length - 1]
 
-  return id
+export function getIdFromUrl(path: string) {
+  const segments = path.split('/')
+  const lastSegment = segments.pop()
+  const lastSegmentWithoutHyphen = lastSegment?.substring(lastSegment.lastIndexOf('-') + 1)
+  return lastSegmentWithoutHyphen
 }
--- src/pages/Category/CategogyDetail.tsx
+++ src/pages/Category/CategogyDetail.tsx
@@ -1,12 +1,25 @@
+import categoryApi from '@/apis/categories.api'
 import { getIdFromUrl } from '@/helpers/common'
-import React from 'react'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export default function CategogyDetail() {
   const location = useLocation()
   const currentPath = location.pathname
+  const id = getIdFromUrl(currentPath)
+  const queryClient = useQueryClient()
 
-  console.log(currentPath, 'currentPath')
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: ['cateDetail'] })
+  }, [location])
+
+  const { data } = useQuery({
+    queryKey: ['cateDetail', currentPath],
+    queryFn: () => categoryApi.getCategorieDetail(id!),
+  })
+
+  console.log(data, '-----------------')
 
   return <div></div>
 }
