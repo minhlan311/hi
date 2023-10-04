0584c13c5a37759adb2b21ed86b613db986a20f4
Hai Tran <haitn2806@gmail.com>
2023-10-04 10:23:52 +0700

    form-gv

--- src/pages/ProfilePage/Certificate/index.tsx
+++ src/pages/ProfilePage/Certificate/index.tsx
@@ -12,7 +12,6 @@ const Certificate = ({ user }: Props) => {
   const dataCertificates = user?.mentorInfo?.certificates
 
   const dilopma = dataDiploma?.filter((item) => item?.schoolName !== 'other' && item?.schoolName !== null)
-  const dilopmaNull = dataDiploma?.filter((item) => item?.schoolName === 'other')
 
   return (
     <Header
@@ -69,28 +68,6 @@ const Certificate = ({ user }: Props) => {
                   />
                 </>
               ))}
-          </div>
-          <Divider />
-          <div>
-            <div className={css.marginTop}>
-              <h3 className={css.h3}>Bằng cấp khác</h3>
-            </div>
-            <div className={css.gridCertificate}>
-              {' '}
-              {dilopmaNull &&
-                dilopmaNull?.map((item) => (
-                  <>
-                    <ImageCustom
-                      styles={{
-                        border: '1px solid',
-                      }}
-                      width='200px'
-                      height='150px'
-                      src={import.meta.env.VITE_FILE_ENDPOINT + '/' + item?.image}
-                    />
-                  </>
-                ))}
-            </div>
           </div>
         </>
       )}
