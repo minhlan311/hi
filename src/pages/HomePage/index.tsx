f5ac6b72f9fdd754bb0cda533344b267473f2a8c
MinhMinh <pnminh1721@gmail.com>
2023-10-23 17:41:00 +0700

    .

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,22 +1,21 @@
 import Mentor from '@/pages/HomePage/Mentor'
 import Banner from './Banner'
+import CourseCalender from './CourseCalender/CourseCalender'
 import Gift from './Gift/Gift'
+import Intro from './Intro'
 import LanguageSystem from './LanguageSystem/LanguageSystem'
+import Mission from './Mission'
+import News from './News/News'
+import TopCourses from './TopCourses'
 import VideoContent from './VideoContent/VideoContent'
 import './styles.scss'
-import TopCourses from './TopCourses'
-import Mission from './Mission'
-import Intro from './Intro'
-import CourseCalender from './CourseCalender/CourseCalender'
-import News from './News/News'
-import CountDownTimer from '@/components/CountDownTimer'
 
 export default function HomePage() {
   return (
     <div className='mtz-homepage'>
-      <CountDownTimer
+      {/* <CountDownTimer
       // timeTillDate='2023-10-23T09:51:35.527Z'
-      />
+      /> */}
       <Banner />
       <Intro />
       <Mentor />
