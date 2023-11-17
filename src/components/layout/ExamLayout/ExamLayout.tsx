4207b75e0058d4314b8293f430b477e5223a5a0e
Hai Tran <haitn2806@gmail.com>
2023-11-17 17:29:29 +0700

    fix ts

--- src/components/layout/ExamLayout/ExamLayout.tsx
+++ src/components/layout/ExamLayout/ExamLayout.tsx
@@ -1,7 +1,7 @@
 import { AppContext } from '@/contexts/app.context'
 import { QuestionCircleOutlined, SoundOutlined } from '@ant-design/icons'
 import { Button, Col, Flex, Layout, Modal, Row, Slider } from 'antd'
-import { Content, Footer, Header } from 'antd/es/layout/layout'
+import { Content, Header } from 'antd/es/layout/layout'
 import { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './ExamLayout.scss'
