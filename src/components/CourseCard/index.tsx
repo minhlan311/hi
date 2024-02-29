77b2ed8c8c37b7dfaa095b23aca98573bae2933a
MinhMinh <pnminh1721@gmail.com>
2024-02-29 11:52:09 +0700

    update time course

--- src/components/CourseCard/index.tsx
+++ src/components/CourseCard/index.tsx
@@ -109,11 +109,11 @@ const CourseCard = ({ item }: Props) => {
                   Lịch học: {!item?.class.length && 'Đang cập nhật'}
                 </Flex>
                 <Flex vertical style={{ marginLeft: 80 }}>
-                  {item?.class.length > 0 && (
+                  {item.class[0]?.event ? (
                     <ul>
                       {item.class[0]?.event?.schedules?.slice(0, 4).map((num, index) =>
                         index < 3 ? (
-                          <li key={num}>{formatDate(item.class[0].event.start, item.class[0].event.start, num)}</li>
+                          <li key={num}>{formatDate(item.class[0]?.event?.start, item.class[0]?.event?.start, num)}</li>
                         ) : (
                           <Popover
                             content={
@@ -121,7 +121,7 @@ const CourseCard = ({ item }: Props) => {
                                 {item.class?.[0]?.event.schedules?.map((num, id) => (
                                   <p key={num}>
                                     {id + 1}.{' '}
-                                    <b>{formatDate(item.class[0].event.start, item.class[0].event.end, num)}</b>
+                                    <b>{formatDate(item.class[0]?.event?.start, item.class[0]?.event?.end, num)}</b>
                                   </p>
                                 ))}
                               </Space>
@@ -133,6 +133,8 @@ const CourseCard = ({ item }: Props) => {
                         ),
                       )}
                     </ul>
+                  ) : (
+                    'Đang cập nhật'
                   )}
                 </Flex>
               </Col>
